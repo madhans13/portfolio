a0327ec51e9505f354fec06871db3cb38ba7722d
Madhan S <madhanklt1331@gmail.com>
2025-07-12 13:45:03 +0530

    moved navbar to the bottom and some improvements

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import styles from './hero.module.css'
 import profileImage from '../../assets/profile.png'
 import ShinyText from '../../blocks/TextAnimations/ShinyText/ShinyText'
+import BlurText from "../../blocks/TextAnimations/BlurText/BlurText";
+const handleAnimationComplete = () => {
+  console.log('Animation completed!');
+};
 const Hero = () => {
   return (
     <section className={styles.hero}>
@@ -10,7 +14,14 @@ const Hero = () => {
         className={styles.profileImage} 
       />
       <h1 className={styles.title}>Hey, I'm Madhan.</h1>
-      <h2 className={styles.subtitle}>Devops & Designer</h2>
+      <h2 className={styles.subtitle}><BlurText
+        text="Devops & Designer"
+        delay={150}
+        animateBy="words"
+        direction="top"
+        onAnimationComplete={handleAnimationComplete}
+        className="text-2xl mb-8"
+      /></h2>
       <p className={styles.description}>
       <ShinyText text="Engineer by degree. Designer by passion. DevOps by drive.
       I build, automate, and design systems that just work — and look good doing it." disabled={false} speed={3} className='custom-class' />
